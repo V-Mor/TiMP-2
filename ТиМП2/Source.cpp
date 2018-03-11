e27692f3d43ce4e12503eb0a01e40273e9ff77b9
V-Mor <vvovv266@gmail.com>
2018-03-11 19:59:28 +0700

    ready to work

--- ТиМП2/Source.cpp
+++ ТиМП2/Source.cpp
@@ -5,28 +5,5 @@
 using namespace std;
 int main()
 {
-	setlocale(0, "");
-	Deq<int> myDeq(10);
-	for (int i = 1; i <= 5; i++)
-	{
-		myDeq.setToHead(i);
-		myDeq.setToTail(100 - i);
-	}
-	cout << myDeq.getFromHead() << endl;
-	cout << myDeq.getFromTail() << endl;
-	for (int i = 1; i <= 8; i++)
-		cout << myDeq.getFromTail() << endl;
-	cout << myDeq.isEmpty() << endl;
-
-
-	for (int i = 1; i <= 5; i++)
-	{
-		myDeq.setToHead(i);
-		myDeq.setToTail(100 - i);
-	}
-	myDeq.clear();
-	cout << myDeq.isEmpty() << endl;
-
-	myDeq.~Deq();
 	return 0;
 }
